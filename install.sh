81ddb3c1703eba721c0c555f3feea56771f7250f
Victor K <ktrace@yandex.ru>
2021-06-26 17:13:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,5 +10,11 @@ patch -p0 < ZFSPlugin.pm.patch
 cd /usr/share/pve-manager/js
 patch -p0 < pvemanagerlib.js.patch
 
-#pveproxy reload
+systemctl restart pvedaemon #pvedaemon reload
+systemctl restart pveproxy #pveproxy reload
+systemctl restart pvestatd
+
+echo "Logout from PVE webgui and clean the browser cache and login again."
+echo "Ctld should be available as a iSCSI provider without restart any node or cluster service."
+
 #refresh config?
